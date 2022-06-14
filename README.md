50fd8e553148facfcfce255787c4819567e26089
Xiaoyancg <xiaoyan@realcodecg.com>
2022-06-14 16:26:08 -0400

    switch to express and add debug

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # MDBlog
 Minimum Markdown text-only blog server.
 
-based on node.js.
+based on node.js
+using [express](https://expressjs.com/), [debug](https://www.npmjs.com/package/debug)
--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-const http = require("http");
-const Markdown = require("markdown-to-html/lib/markdown");
+const debug = require("debug");
+const dr = debug("server")
+
+const express = require("express")
+const app = express();
+const path = require("path")
+
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
-var markdown = require("markdown-to-html").Markdown;
-var md = new Markdown();
+//const Markdown = require("markdown-to-html/lib/markdown");
+//var markdown = require("markdown-to-html").Markdown;
+//var md = new Markdown();
 
+app.get("/", function(req, res) {
+    dr(req.hostname);
+    res.sendFile(__dirname + "doc/" + "index.html");
+});
 
-const requestListener = function(req, res) {
-    req.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello, World!");
-};
+app.listen(port, hostname, function(){
+    dr(`Server is listening at ${hostname}:${port}.`);
+})
 
-const server = http.createServer(requestListener);
-server.listen(port, hostname, function(){console.log(`Server is running at http://${hostname}:${port}`)});
